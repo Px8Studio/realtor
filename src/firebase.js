25dd61bf0cbcefd9b9d98af34dd6a26d76bbdf55
Px8Studio <info@px8.studio>
2025-06-22 21:47:26 +0200

    feat: add Firebase Authentication initialization and export auth reference

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -62,5 +63,8 @@ try {
   // Create a dummy db object to prevent app crashes
   db = null;
 }
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
 
 export { db };
